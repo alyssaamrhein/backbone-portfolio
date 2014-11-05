23fb6b97d8b3513ab5b9bd3eec1cacf843c38056
Alyssa <alyssa@thepalesnail.com>
2014-11-05 16:07:22 -0500

    added a portAdmin view

---
+++ test/views/portAdmin.spec.js
@@ -0,0 +1,10 @@
+/*global beforeEach, describe, it, assert, expect  */
+'use strict';
+
+describe('PortAdmin View', function () {
+
+    beforeEach(function () {
+        this.PortAdminView = new BackbonePortfolio.Views.PortAdmin();
+    });
+
+});
